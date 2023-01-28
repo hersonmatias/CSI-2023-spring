6b7f2240bb0511721e3bf7479f818122783890ad
Herson_Matias <94094990+hersonmatias@users.noreply.github.com>
2023-01-27 18:24:47 -0600

    add

--- zylabs/week 2/zylabs_HMS_to_Sec.cpp
+++ zylabs/week 2/zylabs_HMS_to_Sec.cpp
@@ -1,3 +1,14 @@
+/*Write a program that reads in hours, minutes and seconds as input, and outputs the time in seconds only.
+
+Ex: If the input is:
+
+1 6 40
+
+where 1 is the number of hours, 6 is the number of minutes, and 40 is the number of seconds, the output is:
+
+Seconds: 4000
+
+*/
 #include <iostream>
 using namespace std;
 
