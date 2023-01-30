dee539eec708e5a010265b83be259f7ac838e9a2
Herson_Matias <94094990+hersonmatias@users.noreply.github.com>
2023-01-30 09:53:08 -0600

    1

---
+++ Lecture work/Week_3/Mon_Jan_30/Expression.cpp
@@ -0,0 +1,37 @@
+/* Warm up (IO)
+    Ask the user for their name
+    Print out a greeting to the user by name
+
+Setup : Declare a string variable to hold a name
+    1. Prompt for name (cout)
+    2. Use "cin" to take input from user and store in an object
+    3. Print greeting with the name (no hard-coding)
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    /*
+    string name;
+
+    cout << "What is your name" << endl;
+
+    cin >> name;
+
+    cout << "Hello, " << name << "!" << endl;
+
+    */
+// Variables and Expressions
+   
+     // Declare
+
+     float myNum; 
+
+     // Initialize
+     
+     //num1 = ;
+
+     // Access
+
+     // Update
+}
+
